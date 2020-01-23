7e50f954e84b323891ac0432fe9a55883935aa73
debauchery1st <trevor.j.martin@gmail.com>
2020-01-22 22:27:47 -0500

    cleanup

--- src/components/AdditionalFeatures.js
+++ src/components/AdditionalFeatures.js
@@ -4,23 +4,22 @@ import AdditionalFeature from "./AdditionalFeature";
 
 const mapStateToProps = state => {
   return {
-    features: state.carReducer.features,
-    additionalFeatures: state.carReducer.additionalFeatures.filter(
-      ft => !state.carReducer.car.features.includes(ft)
-    )
+    additionalFeatures: state.carReducer.additionalFeatures,
+    features: state.carReducer.car.features
   };
 };
 
 const AdditionalFeatures = props => {
-  console.log(props);
   return (
     <div className="content">
       <h4>Additional Features</h4>
       {props.additionalFeatures.length > 0 ? (
         <ol type="1">
-          {props.additionalFeatures.map(item => (
-            <AdditionalFeature key={item.id} feature={item} />
-          ))}
+          {props.additionalFeatures
+            .filter(ft => !props.features.includes(ft))
+            .map(item => (
+              <AdditionalFeature key={item.id} feature={item} />
+            ))}
         </ol>
       ) : (
         <p>Nice looking car!</p>
